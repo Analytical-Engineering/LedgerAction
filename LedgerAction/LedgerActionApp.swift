8509565767d2b50ae21583f711d9755c26295fb1
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-19 13:40:41 -0600

    Initial Commit

---
+++ LedgerAction/LedgerActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerActionApp.swift
+//  LedgerAction
+//
+//  Created by J. DeWeese on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LedgerActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
